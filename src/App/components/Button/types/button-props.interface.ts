617b87a9a57e28db50e7ed838886009de204740c
Sahara <sahara.ap13@gmail.com>
2024-08-19 12:58:15 +0300

    Versioning untracked files

--- src/App/components/Button/types/button-props.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IButtonProps {
-  buttonValue: string;
-  onEqual: () => void;
-  onReset: () => void;
-  onSqrt: (value: string) => void;
-  onButtonClick: (value: string) => void;
-}
-
